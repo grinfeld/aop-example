31860e9e5e46c0b2c5a8731f32f46fa96900b566
Mikhail Grinfeld <grinfeld@gmail.com>
2017-07-31 07:33:44 +0300

    added example with Inheritance

---
+++ src/main/java/com/mikerusoft/aosp/example/AopExampleApplicationWithInheritance.java
@@ -0,0 +1,35 @@
+package com.mikerusoft.aosp.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopExampleApplicationWithInheritance implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopExampleApplicationWithInheritance.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println(employeeService.getEmployee().getName());
+		employeeService.getEmployee().setName("Him");
+        employeeService.setEmployee(new Employee("Hello"));
+        try {
+            employeeService.getEmployee().throwException();
+        } catch (Exception e) {
+		    //e.printStackTrace();
+        }
+	}
+
+	@Bean
+	public Employee getEmployee() {
+	    return new SeniorEmployee("Me");
+    }
+}
---
+++ src/main/java/com/mikerusoft/aosp/example/SeniorEmployee.java
@@ -0,0 +1,14 @@
+package com.mikerusoft.aosp.example;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Grinfeld Mikhail
+ * @since 7/31/2017.
+ */
+@NoArgsConstructor
+public class SeniorEmployee extends Employee {
+    public SeniorEmployee(String name) {
+        super(name);
+    }
+}
